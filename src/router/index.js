4c1595790ca15482b666b3303d141f81b484fc9a
qhx0807 <820240134@qq.com>
2018-05-17 17:19:03 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -5,5 +5,6 @@ import routes from './router.config'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: routes
 })
--- src/router/router.config.js
+++ src/router/router.config.js
@@ -1,9 +1,16 @@
+const Error404 = r => require.ensure([], () => r(require('@/components/error-page/404.vue')))
 const Login = r => require.ensure([], () => r(require('@/components/login/login')))
 const Main = r => require.ensure([], () => r(require('@/components/main/main')))
 const Lock = r => require.ensure([], () => r(require('@/components/main-components/lockscreen/components/locking-page.vue')))
 const Department = r => require.ensure([], () => r(require('@/components/settings/department.vue')))
+const Menu = r => require.ensure([], () => r(require('@/components/settings/menu.vue')))
+const Api = r => require.ensure([], () => r(require('@/components/settings/api.vue')))
 
 const routes = [
+  {
+    path: '*',
+    component: Error404
+  },
   {
     path: '/',
     name: 'Login',
@@ -18,6 +25,16 @@ const routes = [
         path: 'Department',
         name: 'Department',
         component: Department
+      },
+      {
+        path: 'Menu',
+        name: 'Menu',
+        component: Menu
+      },
+      {
+        path: 'Api',
+        name: 'Api',
+        component: Api
       }
     ]
   },
