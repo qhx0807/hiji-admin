f5e3287f5cf695a714f2785fc5556f89a1a40c7e
qhx0807 <820240134@qq.com>
2018-10-18 21:35:20 +0800

    n

--- src/components/web/RecommendGoods.vue
+++ src/components/web/RecommendGoods.vue
@@ -2,7 +2,7 @@
   <div class="box">
     <Card :bordered="false" class="mb10">
       <Row>
-        <Col span="6">
+        <!-- <Col span="6">
           <div class="tips">
             <p style="font-size:14px;">
               <img src="http://cdn.cqyyy.cn/redcmmond.png" alt="">
@@ -10,7 +10,7 @@
               <a @click="onClickAdd"><Icon size="14" type="md-add" />添加商品</a>
             </p>
           </div>
-        </Col>
+        </Col> -->
         <Col span="18">
           <Row>
             <Col span="12">
@@ -26,7 +26,7 @@
                 <Option value="0">通用</Option>
                 <Option v-for="(item, index) in cityList" :key="index" :value="item.id">{{item.areaname}}</Option>
               </Select>
-              <Button style="margin-left:12px">添加到推荐</Button>
+              <Button style="margin-left:12px" :loading="addLoading" @click="onClickSaveAdd">添加到推荐</Button>
             </Col>
           </Row>
         </Col>
@@ -37,12 +37,19 @@
         <Col span="4">
           <Input prefix="ios-search" v-model="searchKeys" placeholder="搜索..."></Input>
         </Col>
+        <!-- <Col span="24" style="padding-top:12px;">
+          <RadioGroup v-model="serchCity">
+            <Radio label="0">全部</Radio>
+            <Radio :label="item.id" v-for="(item, index) in cityList">{{item.areaname}}</Radio>
+          </RadioGroup>
+        </Col> -->
       </Row>
     </Card>
     <Row>
       <Col span="24" class="hot-content">
-      123
+        <Spin fix size="large" v-show="spinShow"></Spin>
       </Col>
+
     </Row>
 
     <Modal v-model="addModal" width="750">
@@ -71,20 +78,28 @@ export default {
       searchKeys: '',
       addModal: false,
       modal_loading: false,
+      spinShow: false,
+      addLoading: false,
       cityList: [],
       hotGoods: [],
       goodsids: '',
       selectType: '1',
-      selectCity: '0'
+      selectCity: '0',
+      serchCity: '0'
     }
   },
   created () {
     this.getCityList()
-    // this.getHotGoodsList()
+    this.getHotGoodsList()
   },
   computed: {
     placeholder () {
       return this.selectType == '1' ? '输入商品ID,以逗号分隔' : '商品分类ID'
+    },
+    sortedList () {
+      return this.hotGoods.sort((a, b) => {
+        return a.order - b.order
+      })
     }
   },
   methods: {
@@ -104,7 +119,7 @@ export default {
       )
     },
     getHotGoodsList () {
-      this.$store.commit('pageLoading', true)
+      this.spinShow = true
       serverApi('/web/hotgoodslist', null,
         response => {
           console.log(response)
@@ -113,11 +128,11 @@ export default {
           } else {
             this.$Message.warning(response.data.msg)
           }
-          this.$store.commit('pageLoading', false)
+          this.spinShow = false
         },
         error => {
           console.log(error)
-          this.$store.commit('pageLoading', false)
+          this.spinShow = false
         }
       )
     },
@@ -125,7 +140,33 @@ export default {
       this.addModal = true
     },
     onClickSaveAdd () {
-
+      let reg = /^[\d,]*$/
+      if (!reg.test(this.goodsids) || !this.goodsids) {
+        this.$Message.warning('输入的id格式不正确，请检查')
+        return false
+      }
+      this.addLoading = true
+      let d = {
+        goodsids: this.goodsids,
+        type: this.selectType,
+        city: this.selectCity
+      }
+      serverApi('/web/hotgoodsadd', d,
+        response => {
+          console.log(response)
+          if (response.data.code === 0) {
+            this.getHotGoodsList()
+            this.$Message.success(response.data.msg)
+          } else {
+            this.$Message.warning(response.data.msg)
+          }
+          this.addLoading = false
+        },
+        error => {
+          console.log(error)
+          this.addLoading = false
+        }
+      )
     },
   }
 }
@@ -143,8 +184,9 @@ export default {
 .hot-content{
   padding: 16px;
   border: 1px solid #dedede;
-  height: 570px;
+  height: 550px;
   overflow: auto;
   background-color: #f8f8f8;
+  position: relative;
 }
 </style>
