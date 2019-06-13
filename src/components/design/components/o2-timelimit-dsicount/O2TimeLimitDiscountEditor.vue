1f6da3f3fc7b23d1a574385f759e20a1511b5a20
qhx0807 <820240134@qq.com>
2019-06-13 21:27:39 +0800

    o2 time limit discount

---
+++ src/components/design/components/o2-timelimit-dsicount/O2TimeLimitDiscountEditor.vue
@@ -0,0 +1,143 @@
+<template>
+  <DesignEditor :desc="desc">
+    <Form :label-width="70">
+      <FormItem label="卡券ID" style="margin-bottom: 12px">
+        <Input v-model="designValue.ids" placeholder="卡券id, 以逗号分隔">
+          <Button :loading="searchLoading" slot="append" @click="onClickQueryGoods">查询</Button>
+        </Input>
+      </FormItem>
+      <FormItem label="列表样式" style="margin-bottom: 10px">
+        <RadioGroup v-model="designValue.listStyle">
+          <Radio :label="1">大图模式</Radio>
+          <Radio :label="2">一行两个</Radio>
+          <Radio :label="3">一行三个</Radio>
+          <Radio :label="4">详细列表</Radio>
+          <Radio :label="5">一大两小</Radio>
+          <Radio :label="6">横向滑动</Radio>
+        </RadioGroup>
+      </FormItem>
+      <FormItem label="页面边距" style="margin-bottom:10px">
+        <Slider v-model="designValue.padding" :max="30"></Slider>
+      </FormItem>
+      <FormItem label="图片间距" style="margin-bottom:10px">
+        <Slider v-model="designValue.spacing" :max="30"></Slider>
+      </FormItem>
+      <FormItem label="卡券样式" style="margin-bottom:10px">
+        <RadioGroup v-model="designValue.itemStyle">
+          <Radio :label="1">无边白底</Radio>
+          <Radio :label="2">卡片投影</Radio>
+          <Radio :label="3">描边白底</Radio>
+          <Radio :label="4">无边透明底</Radio>
+        </RadioGroup>
+      </FormItem>
+      <FormItem label="图片比例" style="margin-bottom:10px">
+        <RadioGroup v-model="designValue.imgScale">
+          <Radio :label="1">1 : 1</Radio>
+          <Radio :label="2">3 : 2</Radio>
+          <Radio :label="3">4 : 3</Radio>
+          <Radio :label="4">16 : 9</Radio>
+        </RadioGroup>
+      </FormItem>
+      <FormItem label="文本样式" style="margin-bottom:10px">
+        <RadioGroup v-model="designValue.textStyle">
+          <Radio :label="1">常规体</Radio>
+          <Radio :label="2">加粗体</Radio>
+        </RadioGroup>
+      </FormItem>
+      <FormItem label="显示内容" style="margin-bottom:10px">
+        <CheckboxGroup v-model="designValue.showContent">
+          <Checkbox label="name">卡券名称</Checkbox>
+          <Checkbox label="address">地址</Checkbox>
+          <Checkbox label="disprice">卡券价格</Checkbox>
+          <Checkbox label="orprice">门市价</Checkbox>
+          <Checkbox label="tags">卡券标签</Checkbox>
+          <Checkbox label="buybtn">购买按钮</Checkbox>
+          <Checkbox label="progress">抢购进度条</Checkbox>
+          <Checkbox label="stock">剩余库存</Checkbox>
+          <Checkbox label="buynum">已抢人数</Checkbox>
+          <Checkbox label="timer">抢购倒计时</Checkbox>
+          <!-- <Checkbox label="desc">卡券描述</Checkbox> -->
+          <Checkbox label="badge">角标</Checkbox>
+        </CheckboxGroup>
+      </FormItem>
+      <FormItem v-show="designValue.showContent.indexOf('buybtn') > -1" label="购买按钮" style="margin-bottom:10px">
+        <RadioGroup v-model="designValue.btnStyle">
+          <Radio :label="1">样式一</Radio>
+          <Radio :label="2">样式二</Radio>
+          <Radio :label="3">样式三</Radio>
+          <Radio :label="4">样式四</Radio>
+          <Radio :label="5">样式五</Radio>
+          <Radio :label="6">样式六</Radio>
+          <Radio :label="7">样式七</Radio>
+          <Radio :label="8">样式八</Radio>
+        </RadioGroup>
+      </FormItem>
+      <FormItem v-show="[3,4,7,8].includes(designValue.btnStyle)" label="按钮文字" style="margin-bottom:10px">
+        <Input size="small" v-model="designValue.btnText" style="width: 100px" :maxlength="4"></Input>
+        <Tooltip content="自定义购买按钮文案，最多输入四个字" placement="top">
+          <Icon type="ios-alert-outline" size="18" />
+        </Tooltip>
+      </FormItem>
+      <FormItem v-show="designValue.showContent.indexOf('badge') > -1" label="商品角标" style="margin-bottom:10px">
+        <RadioGroup v-model="designValue.badgeStyle">
+          <Radio label="new-arrival">新品</Radio>
+          <Radio label="hot-sale">热卖</Radio>
+          <Radio label="new">NEW</Radio>
+          <Radio label="hot">HOT</Radio>
+          <Radio label="custom">自定义</Radio>
+        </RadioGroup>
+      </FormItem>
+      <FormItem v-show="designValue.badgeStyle === 'custom'" style="margin-bottom:10px">
+        <InputWithUpload v-model="designValue.badgeImg" size="small" />
+      </FormItem>
+    </Form>
+  </DesignEditor>
+</template>
+<script>
+import editorMixins from '../../mixins/editorMixins'
+import serverApi from '../../../../axios/index.js'
+import InputWithUpload from '../../common/InputWithUpload.vue'
+export default {
+  name: 'O2Editor',
+  mixins: [editorMixins],
+  components: {
+    InputWithUpload
+  },
+  data () {
+    return {
+      searchLoading: false
+    }
+  },
+  created () {},
+  methods: {
+    onClickQueryGoods () {
+      if (!this.designValue.ids) {
+        this.$Message.warning('请输入商品ID')
+        return false
+      }
+      this.searchLoading = true
+      let d = {
+        ids: this.designValue.ids,
+        type: 'o2-timelimit-discount'
+      }
+      serverApi('/Homepage/waresearch', d,
+        response => {
+          if (response.data.data.length > 0) {
+            this.designValue.items = response.data.data
+          } else {
+            this.$Message.warning('未查询到数据')
+          }
+          this.designValue.items = arr
+        },
+        error => {
+          this.$Message.error(error.toString())
+          this.searchLoading = false
+        }
+      )
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/design/components/o2-timelimit-dsicount/index.js
@@ -0,0 +1,82 @@
+import O2TimeLimitDiscountPreview from './O2TimeLimitDiscountPreview.vue'
+import O2TimeLimitDiscountEditor from './O2TimeLimitDiscountEditor.vue'
+import combine from '../../base/combineComponents'
+const designType = 'o2-timelimit-discount'
+const designDescription = '限时抢购'
+
+const O2TimeLimitDiscount = combine(O2TimeLimitDiscountPreview, O2TimeLimitDiscountEditor, designType, designDescription)
+
+O2TimeLimitDiscount.defaultValue = {
+  ids: '',
+  listStyle: 1,
+  padding: 15,
+  spacing: 10,
+  itemStyle: 1,
+  imgScale: 1,
+  textStyle: 1,
+  showContent: ['name', 'tags', 'disprice', 'orprice', 'address', 'buybtn', 'progress', 'timer', 'stock', 'buynum'],
+  btnStyle: 1,
+  btnText: '马上抢',
+  badgeStyle: 'new-arrival',
+  badgeImg: 'http://cdn.cqyyy.cn/pic/20190613140550.png',
+  items: [
+    {
+      id: '1',
+      name: '这里显示卡券名称，最多显示2行',
+      address: '邻水宏帆广场3楼 咨询电话：15988885642',
+      img: 'http://cdn.cqyyy.cn/pic/20190610172641.png',
+      tags: ['精品套餐', '销售火爆', '回头客多'],
+      disprice: '99',
+      orprice: '129',
+      endtime: '',
+      sales: 10,
+      reaminstock: 17,
+      allstock: 89,
+      byname: '90团120'
+    },
+    {
+      id: '2',
+      name: '这里显示卡券名称，最多显示2行',
+      address: '邻水宏帆广场3楼 咨询电话：15988885642',
+      img: 'http://cdn.cqyyy.cn/pic/20190610172641.png',
+      tags: ['精品套餐', '销售火爆', '回头客多'],
+      disprice: '99',
+      orprice: '129',
+      endtime: '',
+      sales: 10,
+      reaminstock: 17,
+      allstock: 89,
+      byname: '90团120'
+    },
+    {
+      id: '3',
+      name: '这里显示卡券名称，最多显示2行',
+      address: '邻水宏帆广场3楼 咨询电话：15988885642',
+      img: 'http://cdn.cqyyy.cn/pic/20190610172641.png',
+      tags: ['精品套餐', '销售火爆', '回头客多'],
+      disprice: '99',
+      orprice: '129',
+      endtime: '',
+      sales: 10,
+      reaminstock: 17,
+      allstock: 89,
+      byname: '90团120'
+    },
+    {
+      id: '4',
+      name: '这里显示卡券名称，最多显示2行',
+      address: '邻水宏帆广场3楼 咨询电话：15988885642',
+      img: 'http://cdn.cqyyy.cn/pic/20190610172641.png',
+      tags: ['精品套餐', '销售火爆', '回头客多'],
+      disprice: '99',
+      orprice: '129',
+      endtime: '',
+      sales: 10,
+      reaminstock: 17,
+      allstock: 89,
+      byname: '90团120'
+    }
+  ]
+}
+
+export default O2TimeLimitDiscount
